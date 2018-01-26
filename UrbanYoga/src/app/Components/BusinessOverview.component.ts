0225b46fdc8888030d5ee72c03eeac8eb4f53128
balu9186 <balu.bandreddi@gmail.com>
2018-01-26 16:34:03 +0530

    New project modified

--- UrbanYoga/src/app/Components/BusinessOverview.component.ts
+++ UrbanYoga/src/app/Components/BusinessOverview.component.ts
@@ -1,5 +1,8 @@
 import {Component} from "@angular/core";
 import {AssessmentService} from "../Services/assessment.service";
+import {forEach} from "@angular/router/src/utils/collection";
+import {until} from "selenium-webdriver";
+import elementIsSelected = until.elementIsSelected;
 
 @Component({
   templateUrl:'../Components/BusinessOverview.component.html'
@@ -8,12 +11,36 @@ import {AssessmentService} from "../Services/assessment.service";
 export class BusinessOverviewComponent{
   defaultdata:any;
   fulldetails:any;
+  details:any[];
+  hideRows:boolean=true;
   constructor(private _assSer:AssessmentService) {
     this._assSer.getBusinessOverView().subscribe(res => this.defaultdata = res, err => console.log(err));
-
-  }
-
-  ViewDetails(){
     this._assSer.getBusinessOverViewDetails().subscribe(res => this.fulldetails = res, err => console.log(err));
+
   }
+
+  ViewDetails(arg1){
+
+    this.details=[
+      {"PricingOptionID":"","ProgramID":"","Name":"","Sales":""}
+    ];
+
+    for(let i=0;i<this.fulldetails.length;i++)
+    {
+
+      if(this.fulldetails[i].ProgramID == arg1){
+
+        this.details.push(this.fulldetails[i]);
+
+      }
+    }
+
+    this.hideRows = false;
+    }
+
+  HideDetails()
+  {
+    this.hideRows = true;
+  }
+
 }
